71a8b5fe43136cde49221f441ace396f8dc4ce49
Dimitar Bonev <dbonev91@users.noreply.github.com>
2014-11-30 15:26:23 +0200

    Update generalVariables.js

--- js/generalVariables.js
+++ js/generalVariables.js
@@ -12,14 +12,21 @@ var GeneralVariables = (function () {
         clickedSmallImage,
         currentImage,
         IMAGES_DIRECTORY = 'uploads/',
-        DESIGN_DIRECTORY = 'design/';
+        DESIGN_DIRECTORY = 'design/',
+        headers = {
+            "X-Parse-Application-Id":"w8RpxfKV4dvAI9B5mm3hDX0w1D995KKEcycW3sX8",
+            "X-Parse-REST-API-Key":"pJlAQ67ALlzu4yAGXhsJptlbIl5kMUfHdqNNfVFV"
+        },
+        url = "https://api.parse.com/1/classes/Images";
 
     return {
         JSONImageData: JSONImageData,
         clickedSmallImage: clickedSmallImage,
         DESIGN_DIRECTORY: DESIGN_DIRECTORY,
         IMAGES_DIRECTORY: IMAGES_DIRECTORY,
-        currentImage: currentImage
+        currentImage: currentImage,
+        headers: headers,
+        url: url
     }
 
 })();
